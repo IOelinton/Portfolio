92ef8f922d44eb024a5ce47994b360990e3d1d38
IOelinton <oelinton.dearaujo@gmail.com>
2025-02-04 02:12:12 -0300

    feat: novo projeto adicionado

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -11,4 +11,16 @@ export const projectsData = [
     githubLink: "https://github.com/IOelinton/Gerador-de-senhas",
     liveLink: "https://ioelinton.github.io/Gerador-de-senhas/",
   },
+  {
+    id: 2,
+    title: "App de receitas",
+    shortDescription:
+      "Aplicativo de receitas que permite buscar receitas por ingrediente ou categoria.",
+    fullDescription:
+      "Aplicativo de receitas desenvolvido com React que permite buscar receitas por ingrediente ou categoria. A API utilizada é a TheMealDB, que fornece uma vasta coleção de receitas de comida internacional. O projeto inclui testes unitários para garantir a qualidade do código.",
+    technologies: ["React", "css", "html", "React Slick"],
+    image: "imgs/app-receita.png",
+    githubLink: "https://github.com/IOelinton/Gerador-de-senhas",
+    liveLink: "https://ioelinton.github.io/app-de-receitas/#/",
+  },
 ];
